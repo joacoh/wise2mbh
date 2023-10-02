85b76d28d1eeaab8606069b34e7131ff11132df3
joacoh <jhyevenes@outlook.es>
2023-10-02 14:10:43 -0300

    Kcorr test

--- build/lib/wise2mbh/__init__.py
+++ build/lib/wise2mbh/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['wise2mbh']
+__all__ = ['wise2mbh','kcorrections']
 
 from .wise2mbh import kcorr_table,clipping_dist, array_montecarlo, distance_modulus_dist, distance_modulus_z, lumdist_z, w2w3_to_morph, wise_to_logsm, morph_to_bulge_ratio, comp_mbh, get_correction_factor, w1_k_corrected, param_montecarlo
 
