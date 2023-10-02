3f07ea3f76392651a616a1e4fd705dab9f3d56ff
joacoh <jhyevenes@outlook.es>
2023-10-02 14:46:01 -0300

    pkg_resources implemented

--- build/lib/wise2mbh/kcorrections/__init__.py
+++ build/lib/wise2mbh/kcorrections/__init__.py
@@ -1,6 +1,7 @@
+import pkg_resources
 import pandas as pd
 
 # Load your tables here
-ekcorr = pd.read_table('E.kcorrection_fw1_w1w2_w1w3.tbl', delim_whitespace=True)
-skcorr = pd.read_table('Sb.kcorrection_fw1_w1w2_w1w3.tbl', delim_whitespace=True)
-lkcorr = pd.read_table('S0.kcorrection_fw1_w1w2_w1w3.tbl', delim_whitespace=True)
+ekcorr = pd.read_table(pkg_resources.resource_filename('E.kcorrection_fw1_w1w2_w1w3.tbl'), delim_whitespace=True)
+skcorr = pd.read_table(pkg_resources.resource_filename('Sb.kcorrection_fw1_w1w2_w1w3.tbl'), delim_whitespace=True)
+lkcorr = pd.read_table(pkg_resources.resource_filename('S0.kcorrection_fw1_w1w2_w1w3.tbl'), delim_whitespace=True)
