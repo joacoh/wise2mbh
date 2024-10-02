dd0ea37992745987838f3f86b19b3040d6f97894
joacoh <jheryev@gmail.com>
2024-10-02 11:06:57 -0300

    0.8.1: functions in test now imported

--- build/lib/wise2mbh/__init__.py
+++ build/lib/wise2mbh/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['wise2mbh','kcorrections','query']
 
-from .wise2mbh import bulge_to_mbh,w1w2_treshold_qso,kcorr_table,clipping_dist, array_montecarlo, distance_modulus_dist, distance_modulus_z, lumdist_z, w2w3_to_morph, wise_to_logsm, morph_to_bulge_ratio, comp_mbh, get_correction_factor, w1_k_corrected, param_montecarlo, drop_irregulars, keep_first_and_non_capitals, mc_size
+from .wise2mbh import bulge_to_mbh,w1w2_treshold_qso,kcorr_table,clipping_dist, array_montecarlo, distance_modulus_dist, distance_modulus_z, lumdist_z, w2w3_to_morph, wise_to_logsm, morph_to_bulge_ratio, comp_mbh, get_correction_factor, w1_k_corrected, param_montecarlo, drop_irregulars, keep_first_and_non_capitals, mc_size, w3_to_SFR
 from .query import query_ned, xmatch_allwise, simple_xmatch
 
-__version__ = "0.8"
+__version__ = "0.8.1"
